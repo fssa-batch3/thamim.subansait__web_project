b92605be74316a6c755b464851e5c8460839175b
Thamimtommy <116333932+Thamimtommy@users.noreply.github.com>
2023-01-25 14:01:04 +0530

    I try something new
    I add some boot and i add the for loop in js for cards. and created the json.

---
+++ js variable.js
@@ -0,0 +1,41 @@
+// let n = 5
+// let string = "";
+// for(let i = 1; i <=n;i++){
+//     string+= i+ " ";
+// }
+// console.log(string);
+
+// let string = "";
+// for(let i = 5; i >=1;i--){
+//     string+= i+ " ";
+// }
+// console.log(string);
+
+// let n=7;
+// let string = "";
+// for(let i = 1; i <=n; i=i+2){
+//     string+= i+ " ";
+// }
+// console.log(string);
+
+// let n = 4
+// let string = "";
+// // let second=" ";
+// for(let i = 1; i <=1;i++){
+//     string+="    "+i;
+    
+    
+// }
+// console.log(string)
+
+// for(let i = 4; i>=1; i--){
+//     second+= i+" ";
+// }
+// console.log(second)
+
+let n=4;
+let str="";
+let space="-";
+for(let i=1;i<n;i++)
+
+
